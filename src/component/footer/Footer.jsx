f1f3a30a8b02de45bdc467168236a77d6d3bc189
ZahraMaroufi1996 <zahramaroufi07@gmail.com>
2024-03-04 13:29:24 +0330

    add react collection for mock server

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -6,7 +6,7 @@ import classes from "./Footer.module.css";
 import classNames from "classnames";
 
 const Footer = () => {
-  const url = "https://ba09580e-e7a2-4d8f-ac33-1e59e5594f17.mock.pstmn.io";
+  const url = "https://35a474cb-4d59-4846-8b43-fc913daf7a5f.mock.pstmn.io";
   const [time, setTime] = useState({
     hour: 9,
     min: 0,
